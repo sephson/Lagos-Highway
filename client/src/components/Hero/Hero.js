5c9b7a8f97c9fa76dcd2d867e5f30259ef48258f
sephson <disutj@gmail.com>
2021-10-31 14:14:17 +0100

    hero

---
+++ client/src/components/Hero/Hero.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Hero.css";
+
+const Hero = () => {
+  return (
+    <main className="hero">
+      <h1 className="hero-header">
+        Enjoyment for the people, and by the people
+      </h1>
+      <p className="hero-text">
+        Run out of ideas on how to flex ? We are curated hundreds of ideas based
+        on people's flexing experiences.
+      </p>
+    </main>
+  );
+};
+
+export default Hero;
---
+++ client/src/components/Places/Places.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Places.css";
+
+const Places = () => {
+  return <div>
+      
+  </div>;
+};
+
+export default Places;
